a239b60fdf3ad8a9ac0f5967d00e1872db874ffd
jmurphy5613 <jmurphy5613@gmail.com>
2023-08-26 19:37:54 -0400

    stuff

--- content.js
+++ content.js
@@ -15,27 +15,64 @@ function startScraping() {
         return;
     }
 
+    const messagesFormatted = [];
 
+    let messageCount = 0;
 
     const observer = new MutationObserver(mutations => {
+
+        let messageItemList = []
+
         mutations.forEach(mutation => {
             mutation.addedNodes.forEach(addedNode => {
                 if (addedNode.classList && addedNode.classList.contains('messageListItem-ZZ7v6g')) {
-                    const messageContent = addedNode.querySelector('.contents-2MsGLg');
+                    const messageAuthor = addedNode.querySelector('.headerText-2z4IhQ');
+                    const messageContent = addedNode.querySelector('.messageContent-2t3eCI');
+                    if (messageAuthor) {
+                        const messageAuthorName = messageAuthor.textContent.trim();
+                        messageItemList.push({ messageType: 'author', messageAuthorName })
+                    }
                     if (messageContent) {
                         const messageText = messageContent.textContent.trim();
-                        console.log('Message:', messageText);
+                        messageItemList.push({ messageType: 'text', messageText })
+                        messageCount++;
                     }
+
                 }
             });
         });
+
+        console.log("Message Item List:", messageItemList)
+
+        let messageAuthorName = '';
+        let messageText = '';
+        for (const messageItem of messageItemList) {
+            if (messageItem.messageType === 'author' && messageAuthorName !== '') {
+                messagesFormatted.push({ messageAuthorName, messageText })
+                messageAuthorName = messageItem.messageAuthorName;
+                messageText = '';
+            }
+            else if (messageItem.messageType === 'author') {
+                messageAuthorName = messageItem.messageAuthorName;
+            } else {
+                messageText = messageText.concat(messageItem.messageText + ". ");
+            }
+        }
+
     });
 
     observer.observe(conversation, { childList: true, subtree: true });
 
     const scroller = document.querySelector('.scroller-kQBbkU');
 
-    setInterval(() => {
-        scroller.scrollTop -= scroller.scrollHeight;
-    }, 5000);
+    let scrollInterval = setInterval(() => {
+        if(scroller.scrollTop !== 0) {
+            scroller.scrollTop -= scroller.scrollHeight;
+        } else {
+            clearInterval(scrollInterval);
+            console.log("Scrolling finished");
+
+            chrome.runtime.sendMessage({ action: 'scrapingFinished', messagesFormatted, messageCount });
+        }
+    }, 500);
 }
--- popup.js
+++ popup.js
@@ -1,8 +1,24 @@
 document.getElementById('startScraping').addEventListener('click', () => {
+    console.log("Start scraping");
     chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
         const activeTab = tabs[0];
-
         // Send a message to the content script to start scraping
         chrome.tabs.sendMessage(activeTab.id, { action: 'startScraping' });
     });
 });
+
+document.getElementById("demo-data-button").addEventListener('click', () => {
+    console.log("Demo data");
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === 'scrapingFinished') {
+        console.log("Scraping finished");
+        const messagesFormatted = request.messagesFormatted;
+        const messageCount = request.messageCount;
+        
+    }
+});
+
+
+
